90a3996e272145987192e8f0ab48b8cdd6688f08
Eden Mesfun <emesfun@gmail.com>
2018-04-04 17:04:28 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     user = User.new(:username => params[:username], :password => params[:password])
-    if !user.username || !user.password
+    if !user.username
       redirect "/failure"
     elsif !user.password
       redirect "/failure"
