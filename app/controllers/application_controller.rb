3cf0f051c7fb85fceaa4cd95d76867d588ca1f22
Eden Mesfun <emesfun@gmail.com>
2018-04-04 17:13:28 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,7 +41,6 @@ class ApplicationController < Sinatra::Base
   post "/login" do
     user = User.find_by(:username => params[:username])
         binding.pry
-    password = User.find_by(:password => params[:password])
     ##your code here
   end
 
