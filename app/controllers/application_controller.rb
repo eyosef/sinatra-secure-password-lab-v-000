af5b0d1c02ac1b94f1f08706ba38d3abdafd24ed
Eden Mesfun <emesfun@gmail.com>
2018-04-04 17:31:59 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -42,7 +42,7 @@ class ApplicationController < Sinatra::Base
     user = User.find_by(:username => params[:username])
 
     if user && user.authenticate(params[:password])
-      binding.prys
+      binding.pry
       redirect :account
     else
       redirect :failure
