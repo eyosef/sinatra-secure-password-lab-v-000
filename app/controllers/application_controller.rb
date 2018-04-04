3350428a01ec34442245371a534103d6b17fdab0
Eden Mesfun <emesfun@gmail.com>
2018-04-04 16:58:39 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < Sinatra::Base
       binding.pry
       redirect "/failure"
     else
-          user = User.new(:username => params[:username], :password => params[:password])
+      user = User.new(:username => params[:username], :password => params[:password])
       redirect "/login"
     end
   end
